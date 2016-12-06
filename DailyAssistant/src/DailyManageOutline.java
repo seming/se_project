473fbef0c79fe083e5749627bd39889a9731b85e
Jinyoung <al6904@naver.com>
2016-12-06 14:12:40 +0900

    DailyManageOutline modified

--- DailyAssistant/src/DailyManageOutline.java
+++ DailyAssistant/src/DailyManageOutline.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
 
 public abstract class DailyManageOutline {
 	public abstract void add();
 	public abstract void delete();
 	public abstract void viewAllList();
-	public abstract boolean alarmWhenDelete();
-	public abstract boolean alarmWhenCancel();
-	public abstract void setPopUpWindow(String confirmMessage);
+	public boolean alarmWhenDelete(){
+		System.out.println("정말 삭제하시겠습니까?\n 1. 확인 \n 2. 취소\n");
+		return confirm();
+	}
+	public boolean alarmWhenCancel(){
+		System.out.println("취소하시겠습니까?\n 1. 확인 \n 2. 취소\n");
+		return confirm();
+	}
+	public void setPopUpWindow(String confirmMessage){
+		System.out.println(confirmMessage);
+	}
+	
+	public boolean confirm(){
+		Scanner scan = new Scanner(System.in);
+		if(scan.nextInt()==1){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 }
