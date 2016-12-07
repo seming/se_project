95d37044ecb267dde00888a42b7cd8c0a4624b7b
seming <semin.j.0110@gmail.com>
2016-12-07 17:35:23 +0900

    modified AccountChanger class

---
+++ DailyAssistant/src/AccountChanger.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class AccountChanger {
+	
+	public String id;
+	public String password;
+	
+	public AccountChanger(/*TODO Auto-generated method stub*/) {
+		// TODO Auto-generated method stub
+	}
+	
+	public boolean identifyPassword(String password) {
+		String passwordForIdentify;
+		
+		System.out.println("현재 비밀번호를 입력해주세요.");
+		Scanner scan = new Scanner (System.in);
+		passwordForIdentify = scan.nextLine();
+		if (passwordForIdentify.equals(password)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public void changeId() {
+		String newIdForChange;
+		String newIdForIdentify;
+		boolean isIdValueSame = false;
+		
+		do{
+			Scanner scan = new Scanner (System.in);
+			System.out.println("새로운 아이디를 입력해주세요." );
+			newIdForChange = scan.nextLine();
+			System.out.println("확인을 위해 다시 한번 입력해주세요.");
+			newIdForIdentify = scan.nextLine();
+		
+			if(newIdForChange.equals(newIdForIdentify)){
+				id = ChangeToNewValue(newIdForChange);
+				System.out.println("아이디가 성공적으로 변경되었습니다.");
+			}
+			else{
+				System.out.println("입력한 새로운 아이디와 확인용 아이디가 다릅니다.");
+				System.out.println("다시 입력해주세요.");
+			}
+		}while(!isIdValueSame);
+	}
+	
+	public void changePassword(String newPassword) {
+		String newPasswordForChange;
+		String newPasswordForIdentify;
+		boolean isPasswordValueSame = false;
+		
+		do{
+			Scanner scan = new Scanner (System.in);
+			System.out.println("새로운 비밀번호를 입력해주세요." );
+			newPasswordForChange = scan.nextLine();
+			System.out.println("확인을 위해 다시 한번 입력해주세요.");
+			newPasswordForIdentify = scan.nextLine();
+		
+			if(newPasswordForChange.equals(newPasswordForIdentify)){
+				id = ChangeToNewValue(newPasswordForChange);
+				System.out.println("비밀번호가 성공적으로 변경되었습니다.");
+			}
+			else{
+				System.out.println("입력한 새로운 비밀번호와 확인용 아이디가 다릅니다.");
+				System.out.println("다시 입력해주세요.");
+			}
+		}while(!isPasswordValueSame);
+	}
+	
+	public String ChangeToNewValue(String newValue) {
+		return newValue;
+	}
+}
--- DailyAssistant/src/AccountManager.java
+++ DailyAssistant/src/AccountManager.java
@@ -1,29 +1,35 @@
+import java.util.Scanner;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class AccountManager {
-	
+
 	public String id;
 	public String password;
 	
-	public AccountManager(/*TODO Auto-generated method stub*/) {
+	/*public Account() {
 		// TODO Auto-generated method stub
+	}*/
+	
+	public void getAccountInformationForLogin() {
+		Scanner scan = new Scanner (System.in);
+		
+		System.out.println("아이디를 입력해주세요");
+		id = scan.nextLine();
+		System.out.println("비밀번호를 입력해주세요");
+		password = scan.nextLine();
 	}
 	
-	public int identifyPassword(String password) {
-		// TODO Auto-generated method stub
-		return 1;
+	public void getAccountInformation() {
+		
 	}
 	
-	public int changeId(String newId) {
-		// TODO Auto-generated method stub
-		return 1;
+	public boolean logIn(String id, String password) {
+		
+		return false;
 	}
 	
-	public int changePassword(String newPassword) {
-		// TODO Auto-generated method stub
-		return 1;
-	}
-	
-	public void swap() {
-		// TODO Auto-generated method stub
+	public void signUp() {
+		
 	}
 }
