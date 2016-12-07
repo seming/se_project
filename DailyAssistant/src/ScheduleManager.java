47d82994eade879db89b9367797639c7eb7dcb14
Jinyoung <al6904@naver.com>
2016-12-07 09:59:54 +0900

    Main method for ScheduleManager

--- DailyAssistant/src/ScheduleManager.java
+++ DailyAssistant/src/ScheduleManager.java
@@ -1,24 +1,38 @@
-import java.util.Vector;
+import java.util.Scanner;
 
 public class ScheduleManager extends DailyManageOutline {
-
 	public String user_id;
-	private Vector<Schedule> scheduleData;
+	private Schedule[] scheduleData;
 	
-	public ScheduleManager(/*TO BE FILLED*/) {
-		
+	public ScheduleManager(String userID) {
+		if(scheduleData==null) {
+			System.out.println("일정이 없습니다");
+			System.out.println("1. 일정추가 \n2. 일정삭제");
+			Scanner scan = new Scanner(System.in);
+			int userAnswer = scan.nextInt();
+			if(userAnswer == 1){
+				//add(userID);
+			}
+		}
+		else {
+			//viewAllList(userID);
+			System.out.println("1. 일정추가 \n2. 일정삭제");
+		}		
 	}
 
+	@Override
 	public void add() {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Override
 	public void delete() {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Override
 	public void viewAllList() {
 		// TODO Auto-generated method stub
 		
