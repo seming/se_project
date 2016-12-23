f416da6f87bd22ff160844c4bff782947c04ee2c
HeaIn <rosum999@naver.com>
2016-12-23 21:28:20 +0900

    changed PhonebookManagerTest into Test

--- DailyAssistant/src/Test.java
+++ DailyAssistant/src/Test.java
@@ -1,9 +1,7 @@
 import static org.junit.Assert.*;
-import org.junit.Test;
-
-public class PhonebookManagerTest {
+public class Test {
 	
-	@Test
+	@org.junit.Test
 	public void testCheckNumberAndModifyIfOutOfRange() {
 		PhonebookManager phonebookManager = new PhonebookManager("0000");
 		assertEquals("01234567891", phonebookManager.checkNumberAndModifyIfOutOfRange("012345678912345"));
@@ -11,7 +9,7 @@ public class PhonebookManagerTest {
 		assertEquals("12341234123", phonebookManager.checkNumberAndModifyIfOutOfRange("1234123412341234"));
 	}
 	
-	@Test
+	@org.junit.Test
 	public void testIsEmptyNameOrEmptyNumber() {
 		PhonebookManager phonebookManager = new PhonebookManager("0000");
 		assertFalse(phonebookManager.isEmptyNameOrEmptyNumber("asdf", "02123456780"));
